17c796bab3c11ea6a11cab23a0e821ee2925d878
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-10 14:41:09 -0800

    Updated Article model for id

--- models/Articles.js
+++ models/Articles.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
+    _id: { 
+        type: Schema.Types.ObjectId, 
+        required: true 
+    },
     title: {
         type: String,
         required: true
