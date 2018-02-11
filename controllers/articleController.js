f15e6b689d293c32bfd5d44ae52eb363694c566f
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-11 13:50:43 -0800

    Setup find methods in article controller

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,2 +1,17 @@
 const db = require("../models");
 
+module.exports = {
+    findAll: function(req, res) {
+        db.Article
+            .find(req.query)
+            .sort({date:-1})
+            .then(dbArticle => res.json(dbArticle))
+            .catch(err => res.status(418).json(err));
+    },
+    findById: function(req, res) {
+        db.Article
+            .findById(req.params.id)
+            .then(dbArticle => res.json(dbArticle))
+            .catch(err => res.status(418).json(err));
+    }
+};
