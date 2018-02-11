35d565967b72a08b46bb76b3333db1d940f8d91e
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-10 18:36:13 -0800

    Updated apiController for api call

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,11 +1,25 @@
 const axios = require('axios');
 const db = require('../models');
 
-
+// Defining api methods
 module.exports = {
-    api_key= "ca0f6afae07c412198618d588e6a00f9"
-
-
+    findAll: function (req, res) {
+        const params = Object.assign(
+            { api_key= "ca0f6afae07c412198618d588e6a00f9" },
+            req.query
+        );
+        axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", 
+        { params })
+        .then(response => {
+            db.Article.find().then(dbArticles => 
+                response.data.docs.filter(article => 
+                    dbArticles.every(dbArticle => 
+                        dbArticle => dbArticle._id.toString() !== article._id
+                    )
+                )
+            ).then(articles => res.json(articles)).catch(err => res.status(418).json(err));
+        });
+    }
 }
 
 
